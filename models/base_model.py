c394986d3ddbe8384d760aabb1c705b846df1425
Miuywu <859@holbertonschool.com>
2019-11-06 22:10:30 +0000

    base class has basic attrs/methods

--- models/base_model.py
+++ models/base_model.py
@@ -1,1 +1,33 @@
-something something
+#!/usr/bin/python3
+from uuid import uuid4
+from datetime import datetime
+class BaseModel():
+    """
+    defines all common attributes/methods for other classes
+    """
+
+    def __init__(self):
+        self.id = str(uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
+
+    def __str__(self):
+        return("[" + self.__class__.__name__ + "] " + "(" + self.id + ") " + str(self.__dict__))
+
+    def save(self):
+        """
+        updates the public instance attribute updated_at with the current datetime
+        """
+        self.updated_at = datetime.now()
+
+    def to_dict(self):
+        """
+        returns a dictionary containing all keys/values of __dict__ of the instance
+        """
+        dic = self.__dict__.copy()
+        dic['__class__'] = self.__class__.__name__
+        dic['created_at'] = self.created_at.isoformat()
+        dic['updated_at'] = self.updated_at.isoformat()
+
+        return dic
+
