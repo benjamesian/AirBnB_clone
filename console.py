c6f2e4bb7bafed59c29e0a9b5956548b875b01f1
Ben Cook <808@holbertonschool.com>
2019-11-10 14:31:54 -0800

    class_name.method in console

--- console.py
+++ console.py
@@ -9,12 +9,34 @@ from models.city import City
 from models.amenity import Amenity
 from models.review import Review
 
+import json
+
 class HBNBCommand(cmd.Cmd):
     """custom cmd for air_bnb"""
 
     prompt = "(hbnb) "
 
     model_names = ['BaseModel', 'User', 'State', 'City', 'Amenity', 'Place', 'Review']
+    cmds = ['all', 'show', 'count', 'destroy', 'update']
+    def precmd(self, line):
+        """Pre command hook"""
+        args_arr = line.split('.', maxsplit=1)
+        if len(args_arr) == 2:
+            model, raw_method = args_arr
+            meth_arr = raw_method.split('(', maxsplit=1)
+            if len(meth_arr) == 2:
+                method, raw_arguments = meth_arr
+                arguments = ' '.join(map(lambda x: x.strip(), raw_arguments.rstrip(')').split(',')))
+                if model in self.model_names:
+                    if method in self.cmds:
+                        if method == 'update' and len(arguments) == 2:
+                            d = json.loads(arguments[1])
+                            for k, v in d.items():
+                                pass
+                        else:
+                            line = method + ' ' + model + ' ' + arguments
+                        # print('custom line:', line)
+        return line
 
     def do_quit(self, args):
         """Quit command to exit the program\n"""
@@ -66,6 +88,19 @@ class HBNBCommand(cmd.Cmd):
                 print('** no instance found **')
             else:
                 print(models.storage.all()[key])
+
+    def do_count(self, args):
+        """Count instances of a specific class"""
+        count = 0
+        if not args:
+            print('** class name missing **')
+        elif args not in self.model_names:
+            print("** class doesn't exist")
+        else:
+            for k in models.storage.all().keys():
+                if k.startswith(args + '.'):
+                    count += 1
+        return count
 
     def do_destroy(self, args):
         """Delete and instance based on class name and id\n"""
@@ -122,11 +157,14 @@ class HBNBCommand(cmd.Cmd):
             else:
                 model = models.storage.all()['.'.join(args_arr[:2])]
                 args_arr[3] = args_arr[3].strip('\"')
-                if args_arr[3].is_digit():
+                if args_arr[3].isdigit():
                     args_arr[3] = int(args_arr[3])
-                elif args_arr[3].is_decimal():
+                elif args_arr[3].isdecimal():
                     args_arr[3] = float(args_arr[3])
                 setattr(model, args_arr[2], args_arr[3])
+
+    def do_User(self, args):
+        return self.do_all('User')
 
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
