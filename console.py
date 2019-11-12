5945d37808adf8fa6695fcb7bce57b716b00c734
Ben Cook <808@holbertonschool.com>
2019-11-12 13:51:35 -0800

    add docsstrings

--- console.py
+++ console.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+"""Console."""
 import cmd
 import models
 from models.base_model import BaseModel
--- models/user.py
+++ models/user.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+"""User model."""
 from models.base_model import BaseModel
 
 
