0a789cf2991e2e0ccba32fe45b7e4aa4ec33f127
Ben Cook <808@holbertonschool.com>
2019-11-12 17:07:16 -0800

    add tests for User model

--- tests/test_models/test_user.py
+++ tests/test_models/test_user.py
@@ -1,1 +1,18 @@
 #!/usr/bin/python3
+import unittest
+from models.user import User
+
+
+class TestState(unittest.TestCase):
+    """testing inherited functionality from BaseModel"""
+
+    def setUp(self):
+        """test obj instantiation"""
+        self.obj = User()
+
+    def test_Attrs(self):
+        """checks the existence of expected attrs"""
+        self.assertTrue(hasattr(self.obj, 'email'))
+        self.assertTrue(hasattr(self.obj, 'password'))
+        self.assertTrue(hasattr(self.obj, 'first_name'))
+        self.assertTrue(hasattr(self.obj, 'last_name'))
